5547f1498fc7f75701bd0a80047337ef0b3ca02e
mehedihasankanon <mehedi.hkanon@gmail.com>
2026-07-26 15:22:37 +0600

    ok ok

--- Term-2/CSE220/2022/Onlines/Online 2 _ Convolution/C/Online_C1_C2/template.py
+++ Term-2/CSE220/2022/Onlines/Online 2 _ Convolution/C/Online_C1_C2/template.py
@@ -1,34 +1,52 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-# Todo: Define Signal class
-        
+from signal_lti import DiscreteSignal, LTISystem
+
+
+# Helper class to represent a linear combination of signals
 class SuperSignal:
     def __init__(self):
-        self.components = []
+        self.components = []  # Stores tuples of (coefficient, DiscreteSignal)
 
-    def add(self, signal: Signal, coefficient=1.0):
+    def add(self, signal: DiscreteSignal, coefficient=1.0):
         self.components.append((coefficient, signal))
-        
-# Todo: Define LTI class
+
 
 if __name__ == "__main__":
-    INF = 10
+    # ---- 1. Component Signals ----
+    # x1[n] = 1 at n = 0
+    x1 = DiscreteSignal(0, 0)
+    x1.set_value_at_time(0, 1.0)
 
-    # Component signals
-    x1 = Signal(INF)
-    x1.set_value_at_time(0, 1)
+    # x2[n] = 1 at n = 2
+    x2 = DiscreteSignal(2, 2)
+    x2.set_value_at_time(2, 1.0)
 
-    x2 = Signal(INF)
-    x2.set_value_at_time(2, 1)
+    # ---- TODO 1: Create SuperSignal for x[n] = 2*x1[n] - x2[n] ----
+    super_x = SuperSignal()
+    # Hint: Use super_x.add(x1, 2.0) and super_x.add(x2, -1.0)
+    super_x.add(x1, 2.0)
+    super_x.add(x1, -1.0)
 
-    # Todo: Create SuperSignal: x(n) = 2*x1(n) - x2(n)
-
-    # Impulse response
-    h = Signal(INF)
-    h.set_value_at_time(0, 1)
+    # ---- 2. Impulse Response h[n] ----
+    # h[n] = 1 at n = 0, 0.5 at n = 1
+    h = DiscreteSignal(0, 1)
+    h.set_value_at_time(0, 1.0)
     h.set_value_at_time(1, 0.5)
 
-    system = LTI_System(h)
+    system = LTISystem(h)
 
-    # Todo: Output using superposition
+    # ---- TODO 2: Compute Output using Superposition ----
+    # By Linearity of LTI systems:
+    # If y_i[n] is the system output for input x_i[n], 
+    # then total output y[n] = sum( c_i * system.output(x_i) )
+    
+    # y_superposition = ...
+    
+    
+    # ---- TODO 3: Verify against Direct Convolution Output ----
+    # Hint: Reconstruct full input signal x = x1.multiply(2.0).add(x2.multiply(-1.0))
+    # y_direct = system.output(x)
+    
+    # print("Superposition and Direct outputs match:", np.allclose(y_superposition.values, y_direct.values))
